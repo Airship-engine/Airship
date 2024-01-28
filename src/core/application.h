c933fb884640853822590c9793f714bc9a982007
Simonsays095 <simonsays095@gmail.com>
2024-01-28 15:45:21 -0800

    separate library from test application

--- src/core/application.h
+++ src/core/application.h
@@ -28,29 +28,9 @@ namespace Airship {
             return m_Windows.emplace_back(std::make_unique<Window>(w, h)).get();
         }
 
-        virtual void OnStart() {};
+        virtual void OnStart() {}
 
     private:
         std::vector<std::unique_ptr<Window>> m_Windows;
     };
-}
-
-class GameClass : public Airship::Application
-{
-protected:
-    void OnStart() override;
-};
-
-void GameClass::OnStart()
-{
-    std::optional<Airship::Window*> win = CreateWindow(800, 600);
-    assert(win.has_value());
-
-    std::cout << "Could render text now" << std::endl;
-}
-
-int main()
-{ 
-    GameClass app;
-    app.Run();
 }
