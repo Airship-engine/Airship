e4d0c85fe6bfb6639e47c03ad35cbd1819a0528b
Simonsays095 <simonsays095@gmail.com>
2024-02-04 22:59:11 -0800

    update testing files

--- test/testapp.cpp
+++ test/testapp.cpp
@@ -1,22 +1,6 @@
 #include "core/application.h"
 #include "gtest/gtest.h"
-
-class GameClass : public Airship::Application
-{
-public:
-    GameClass() = default;
-    GameClass(int height, int width) : height(height), width(width) {}
-    const std::optional<Airship::Window*> GetWindow() const { return win; }
-protected:
-    void OnStart() override;
-    int height = 800, width = 600;
-    std::optional<Airship::Window*> win;
-};
-
-void GameClass::OnStart()
-{
-    win = CreateWindow(width, height);
-}
+#include "common.h"
 
 TEST(Window, nonnull) {
     GameClass app;
