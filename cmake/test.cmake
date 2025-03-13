6d5d3485293868a70f0eee3d763781e124bdae89
Elliott Ewing <EQEwing@gmail.com>
2025-03-12 22:19:46 -0700

    Add cmake function to compile multiple tests into one executable

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -1,4 +1,31 @@
 
+function(airship_test TARGET_NAME SOURCE_LIST)
+    # Optional argument DISABLE_ENVIRONMENT disables the airship testbed for sensitive tests that knowingly go into bad states.
+    # Useful for testing logging validity, or recovery from normally fatal situations.
+    set(options DISABLE_ENVIRONMENT)
+    cmake_parse_arguments(airship_test "${options}" "" "" ${ARGN})
+
+    if(BUILD_TESTS)
+        add_executable(${TARGET_NAME} "${SOURCE_LIST}")
+
+        target_link_libraries(${TARGET_NAME}
+            PRIVATE
+                AirshipRenderer
+                AirshipTest
+                core
+                gtest_main
+        )
+        target_include_directories(${TARGET_NAME}
+            PRIVATE
+                ${PROJECT_SOURCE_DIR}/test/include
+        )
+        if(airship_test_DISABLE_ENVIRONMENT)
+            target_compile_definitions(${TARGET_NAME} PUBLIC DISABLE_TEST_ENVIRONMENT=1)
+        endif()
+        gtest_discover_tests(${TARGET_NAME})
+	endif()
+endfunction()
+
 #   Configure unit tests    #
 function(add_airship_test SOURCE)
     if(BUILD_TESTS)
