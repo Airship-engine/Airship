cd3af43a7f94d63d84b1de37c7964f408ca55f4f
Elliott Ewing <EQEwing@gmail.com>
2025-12-30 20:39:05 -0800

    Organize project includes for Visual Studio targets view

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,13 @@
+function(enable_clang_tidy tgt)
+    if(CLANG_TIDY)
+        find_program(CLANG_TIDY_EXE NAMES "clang-tidy")
+        if(CLANG_TIDY_EXE)
+            message(STATUS "Found clang-tidy: ${CLANG_TIDY_EXE}")
+            set_target_properties(${tgt} PROPERTIES
+                CXX_CLANG_TIDY "${CLANG_TIDY_EXE}"
+            )
+        else()
+            message(WARNING "clang-tidy not found!")
+        endif()
+    endif()
+endfunction()
